e650e267fc2cd01824db8bef97a35adf048ce726
Aaron Burtnyk <aaron.burtnyk@candide.eu>
2019-12-06 13:37:51 +0000

    Add verbose mode

--- readme.md
+++ readme.md
@@ -12,7 +12,8 @@ node src/cli.js --tags "@1 or @2"
 node src/cli.js --tags "@2 and @3"
 node src/cli.js --tags "@1 and not @2"
 node src/cli.js --tags "(@1 or @2) and @3"
-node src/cli.js --configPath "test/pv3.json"
+node src/cli.js --configPath "test/pv3/pv3.json"
+node src/cli.js --configPath "test/pv3/pv3.json" --verbose
 ```
 
 ### Config File
@@ -20,12 +21,12 @@ node src/cli.js --configPath "test/pv3.json"
 ```
 {
   "basePath": "test/ui",
-  "tags": "@2 and @3"
+  "tags": "@2 and @3",
+  "verbose": true
 }
 ```
 
 ## Todo
 
-verbose mode
 typescript
 output formatting
--- src/cli.js
+++ src/cli.js
@@ -7,14 +7,6 @@ const commandLineArgs = require("command-line-args")
 const constructCucumberArgs = require("./construct-cucumber-args")
 const {projectName} = require("./constants")
 
-const optionDefinitions = [
-  {name: "tags", type: String},
-  {name: "feature", type: String},
-  {name: "configPath", type: String},
-]
-
-const parsedArgs = commandLineArgs(optionDefinitions, {partial: true})
-
 function getConfigPath(args) {
   if (args.configPath && fs.existsSync(args.configPath)) return args.configPath
 
@@ -24,36 +16,56 @@ function getConfigPath(args) {
   return null
 }
 
-function getConfig(path) {
-  const contents = fs.readFileSync(path, "utf8")
-  return parseAndValidateJSON(contents, path)
-}
+function getConfig(args) {
+  const path = getConfigPath(args)
 
-function parseAndValidateJSON(config, path) {
+  if (!path) {
+    if (isVerbose(args)) {
+      console.log("No configuration path specified")
+    }
+    return null
+  }
+
+  const contents = fs.readFileSync(path, "utf8")
+
   try {
-    return JSON.parse(config)
+    const parsed = JSON.parse(contents)
+
+    if (isVerbose(args, parsed)) {
+      console.log(`Configuration loaded from ${path}`)
+    }
+
+    return parsed
   } catch (err) {
-    throw new Error(path + " does not include a valid JSON object.\n")
+    throw new Error(`${path} does not include a valid JSON object`)
   }
 }
 
-const configPath = getConfigPath(parsedArgs)
-const config = configPath ? getConfig(parsedArgs) : null
+function isVerbose(args, config = {}) {
+  if (args.verbose) return args.verbose && args.verbose !== "false"
+  return !!config.verbose
+}
+
+const optionDefinitions = [
+  {name: "tags", type: String},
+  {name: "feature", type: String},
+  {name: "configPath", type: String},
+  {name: "verbose", type: Boolean},
+]
+
+const parsedArgs = commandLineArgs(optionDefinitions, {partial: true})
 
 const cucumberArgs = process.argv
   .slice(0, 2)
-  .concat(constructCucumberArgs(parsedArgs, config))
+  .concat(constructCucumberArgs(parsedArgs, getConfig(parsedArgs)))
 
-console.log("process.argv", process.argv)
-console.log("parsedArgs", parsedArgs)
-console.log("configPath", configPath)
-console.log("config", config)
-console.log("cucumberArgs", cucumberArgs)
-
-const result = new Cucumber.Cli({
+new Cucumber.Cli({
   argv: cucumberArgs,
   cwd: process.cwd(),
   stdout: process.stdout,
-}).run()
-
-console.log("result", result)
+})
+  .run()
+  .then(success => {
+    console.log("success", success)
+    process.exit(success ? 0 : 1)
+  })
